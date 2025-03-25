d61fc46d2bf8a728f676ad7d5c9cf3b677ccf1a6
victorrh1 <victorwow7@outlook.com>
2025-03-24 23:59:51 -0300

    update

--- app.py
+++ app.py
@@ -4,11 +4,11 @@ import os
 from time import sleep
 
 driver = webdriver.Chrome()
-driver.get('https://devaprender-play.netlify.app/')
+driver.get('https://clone-olx-devaprender.netlify.app/')
 sleep(5)
 
-produtos = driver.find_elements(By.XPATH,"//h3[@class='text-lg font-semibold text-gray-900 group-hover:text-indigo-600']")
-precos = driver.find_elements(By.XPATH,"//p [@class='text-2xl font-bold text-indigo-600']")
+produtos = driver.find_elements(By.XPATH,"//h3[@class='text-base text-gray-900 line-clamp-2 mb-1 hover:text-[#6E0AD6]']")
+precos = driver.find_elements(By.XPATH,"//span [@class='text-2xl font-bold text-gray-900']")
 
 for produto, preco in zip(produtos,precos):
 	with open('preços.csv', 'a',encoding='utf-8') as arquivo:
